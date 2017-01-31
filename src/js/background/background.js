0fc3aa75a46de30adbe06fcb9633510c257d50a4
Asaf Menahem <asimen1@gmail.com>
2017-01-31 09:36:16 +0200

    background - wip2

--- src/js/background/background.js
+++ src/js/background/background.js
@@ -4,6 +4,10 @@ import Messenger from 'chrome-ext-messenger';
 let messenger = new Messenger();
 window.m = messenger;
 
+chrome.tabs.onCreated.addListener(function(tab) {
+	console.log('tab created:', tab, tab.id);
+});
+
 messenger.initBackgroundHub();
 
 let messageHandler = function(message, sender, sendResponse) {
@@ -13,13 +17,59 @@ let messageHandler = function(message, sender, sendResponse) {
 
 window.c = messenger.initConnection('main', messageHandler);
 
-// let messageHandler2 = function(message, sender, sendResponse) {
-//     console.log('background 2 got message:', arguments);
-//     sendResponse('I AM RESPONSE FROM BACKGROUND 2');
-// };
+let messageHandler2 = function(message, sender, sendResponse) {
+    console.log('background 2 got message:', arguments);
+    sendResponse('I AM RESPONSE FROM BACKGROUND 2');
+};
 
-// window.c2 = messenger.initConnection('main2', messageHandler2);
+window.c2 = messenger.initConnection('main2', messageHandler2);
 
-window.runTests = function() {
-    //...
+window.runTests = function(tabId) {
+    console.log('BACKGROUND TO CONTENT SCRIPT:');
+    console.log('--- main to main2 --- ');
+    window.c.sendMessage('content_script:main2:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('--- main to main --- ');
+    window.c.sendMessage('content_script:main:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('--- main2 to main --- ');
+    window.c2.sendMessage('content_script:main2:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('--- main to main,main2 --- ');
+    window.c.sendMessage('content_script:main,main2:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('--- main to * --- ');
+    window.c.sendMessage('content_script:*:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('BACKGROUND TO POPUP:');
+    console.log('--- main to main --- ');
+    window.c.sendMessage('popup:main:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('--- main to main --- ');
+    window.c.sendMessage('popup:main:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('--- main2 to main --- ');
+    window.c2.sendMessage('popup:main:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('--- main to main,main2 --- ');
+    window.c.sendMessage('popup:main,main2:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('--- main to * --- ');
+    window.c.sendMessage('popup:*:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('BACKGROUND TO DEVTOOL:');
+    console.log('--- main to main --- ');
+    window.c.sendMessage('devtool:main:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('--- main to main --- ');
+    window.c.sendMessage('devtool:main:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('--- main2 to main --- ');
+    window.c2.sendMessage('devtool:main:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('--- main to main,main2 --- ');
+    window.c.sendMessage('devtool:main,main2:' + tabId, 'some message', function(res) { console.log('got response:', res); });
+
+    console.log('--- main to * --- ');
+    window.c.sendMessage('devtool:*:' + tabId, 'some message', function(res) { console.log('got response:', res); });
 };
